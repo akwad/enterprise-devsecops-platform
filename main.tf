ce5f0a10d52a31d33d1e2f3f50fc45dfe25e0ee1
Hussein Akinwande <hussein@hack-thing.info>
2026-07-24 10:16:16 -0400

    Add Terraform remote state bootstrap

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = "enterprise-devsecops-tfstate-hussein"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+
+
+  versioning_configuration {
+
+    status = "Enabled"
+
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+
+
+  rule {
+
+    apply_server_side_encryption_by_default {
+
+      sse_algorithm = "AES256"
+
+    }
+
+  }
+
+}
+
+
